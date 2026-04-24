f66603ef155716e9474437145328c8b5b72a6628
Shaurya Shekhawat <shauryashekhawat007@gmail.com>
2026-04-24 14:41:16 +0530

    first commit

--- student_class.py
+++ student_class.py
@@ -5,19 +5,19 @@
 # method: display_details()
 
 
-class Student:
-    def __init__(self,name,age,marks):
-        self.name = name
-        self.age = age 
-        self.marks = marks 
+# class Student:
+#     def __init__(self,name,age,marks):
+#         self.name = name
+#         self.age = age 
+#         self.marks = marks 
 
-    def display(self):
-        print(self.name)
-        print(self.age)
-        print(self.marks)
+#     def display(self):
+#         print(self.name)
+#         print(self.age)
+#         print(self.marks)
 
-x = Student("ajay",21,67)
-x.display()
+# x = Student("ajay",21,67)
+# x.display()
 
 # ====================================================================================================================================================================
 
@@ -31,28 +31,28 @@ x.display()
 
 # stop()
 
-class car:
-    def __init__(self,brand,speed):
-        self.brand = brand
-        self.speed = speed
+# class car:
+#     def __init__(self,brand,speed):
+#         self.brand = brand
+#         self.speed = speed
 
-    def start(self):
-        print(" Your car name is " + self.brand , "now start your car")
+#     def start(self):
+#         print(" Your car name is " + self.brand , "now start your car")
 
-    def accelerate(self):
-        print(" Put your leg on accelerator and drive the car with speed " , self.speed )
+#     def accelerate(self):
+#         print(" Put your leg on accelerator and drive the car with speed " , self.speed )
     
-    def stop(self):
-        print(" stop the car ")
+#     def stop(self):
+#         print(" stop the car ")
 
-    def display(self):
-        print(self.brand)
-        print(self.speed)
+#     def display(self):
+#         print(self.brand)
+#         print(self.speed)
 
-value = car("BMW M5 COMPETETION", 100)
-value.start()
-value.accelerate()
-value.stop()
+# value = car("BMW M5 COMPETETION", 100)
+# value.start()
+# value.accelerate()
+# value.stop()
 
 
 # ==================================================================================================================================================================
@@ -67,24 +67,24 @@ value.stop()
 
 # perimeter()
 
-class rectangle:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
+# class rectangle:
+#     def __init__(self, length, width):
+#         self.length = length
+#         self.width = width
      
-    def area(self):
-        print(self.length)
+#     def area(self):
+#         print(self.length)
 
-    def perimeter(self):
-        print(self.width)
+#     def perimeter(self):
+#         print(self.width)
          
-    def display(self):
-        print(self.length)
-        print(self.width)
+#     def display(self):
+#         print(self.length)
+#         print(self.width)
 
-x = rectangle(4 , 7)
-x.area()
-x.perimeter()
+# x = rectangle(4 , 7)
+# x.area()
+# x.perimeter()
 
 # =====================================================================================================================================================================
 
@@ -98,25 +98,25 @@ x.perimeter()
 
 # divide(a, b)
 
-class calculator:
+# class calculator:
     
-    def add(self, a,b):
-        return a+b
+#     def add(self, a,b):
+#         return a+b
 
-    def subtract(self, a , b):
-        return a-b
+#     def subtract(self, a , b):
+#         return a-b
     
-    def multiply(self, a , b):
-      return a * b
+#     def multiply(self, a , b):
+#       return a * b
 
-    def divide(self,a,b):
-        return a/b
-calc = calculator()
+#     def divide(self,a,b):
+#         return a/b
+# calc = calculator()
     
-print(calc.add(10, 5))
-print(calc.subtract(10, 5))
-print(calc.multiply(10, 5))
-print(calc.divide(10, 5))
+# print(calc.add(10, 5))
+# print(calc.subtract(10, 5))
+# print(calc.multiply(10, 5))
+# print(calc.divide(10, 5))
 
 
 # =====================================================================================================================================================================
@@ -139,86 +139,86 @@ print(calc.divide(10, 5))
 # • Print messages after each transaction
 
 
-class Bankaccount:
+# class Bankaccount:
 
     
     
-    def __init__(self,name,balance):
-        self.name = name 
-        self.balance = balance 
+#     def __init__(self,name,balance):
+#         self.name = name 
+#         self.balance = balance 
      
 
-    def deposit(self, amount):     
-        print("deposit amount is" ,amount)
-        self.balance = self.balance + amount
-        self.mybalance()
+#     def deposit(self, amount):     
+#         print("deposit amount is" ,amount)
+#         self.balance = self.balance + amount
+#         self.mybalance()
 
-    def withdraw(self,value):
-        if value > self.balance:
-            print("insufficiant balance withdraw is cancled")
-        else:
-            print("withdraw amount", value)
-            self.balance = self.balance - value
-        self.mybalance()
+#     def withdraw(self,value):
+#         if value > self.balance:
+#             print("insufficiant balance withdraw is cancled")
+#         else:
+#             print("withdraw amount", value)
+#             self.balance = self.balance - value
+#         self.mybalance()
 
-    def mybalance(self):
-        print("your account balance is ", self.balance)
+#     def mybalance(self):
+#         print("your account balance is ", self.balance)
 
-x = Bankaccount("Shaurya", 20000)
+# x = Bankaccount("Shaurya", 20000)
 
-print(x.name)
-print(x.balance)
-x.deposit(3000)
-x.withdraw(4000)
+# print(x.name)
+# print(x.balance)
+# x.deposit(3000)
+# x.withdraw(4000)
 
 # ===================================================================
 
-class student:
-    def __init__(self,name,age,section,marks):
-        self.name = name
-        self.age = age 
-        self.section = section
-        self.marks = marks 
+# class student:
+#     def __init__(self,name,age,section,marks):
+#         self.name = name
+#         self.age = age 
+#         self.section = section
+#         self.marks = marks 
 
-    def display(self):
-        print(self.name)
-        print(self.age)
-        print(self.section)
-        print(self.marks)
+#     def display(self):
+#         print(self.name)
+#         print(self.age)
+#         print(self.section)
+#         print(self.marks)
 
-    def ispass(self):
-        if self.marks >= 40:
-            print("Pass")
+#     def ispass(self):
+#         if self.marks >= 40:
+#             print("Pass")
 
-        else:
-            print("fail")   
+#         else:
+#             print("fail")   
 
-x = student("shaurya",18,"B",45)
-y = student("naman",45,"a",30)
-x.display()
-x.ispass()
-y.display()
-y.ispass()
+# x = student("shaurya",18,"B",45)
+# y = student("naman",45,"a",30)
+# x.display()
+# x.ispass()
+# y.display()
+# y.ispass()
 
 # =========================================================================
 
 
-class student:
-    def __init__(self,marks):
-        self.marks = marks
+# class student:
+#     def __init__(self,marks):
+#         self.marks = marks
          
-    def mark(self,marks):
-        if marks > self.marks:
-            print("pass")
+#     def mark(self,marks):
+#         if marks > self.marks:
+#             print("pass")
         
-        else:
-            print("fail")
+#         else:
+#             print("fail")
 
-    def display(self):
-        print(self.marks)
+#     def display(self):
+#         print(self.marks)
 
-x = student(46)
-x.display()
+# x = student(46)
+# x.display()
 
 # ================================================================================================================================================================
 
@@ -232,28 +232,138 @@ x.display()
 # remove_item(item_name)
 # show_items()
 
-class ShoppingCart:
-    def __init__(self):
-        self.items = []   
+# class ShoppingCart:
+#     def __init__(self):
+#         self.items = []   
 
-    def add_item(self, item_name):
-        self.items.append(item_name)
-        print(item_name + " added ")
+#     def add_item(self, item_name):
+#         self.items.append(item_name)
+#         print(item_name + " added ")
 
-    def remove_item(self, item_name):
-            self.items.remove(item_name)
-            print(item_name + " removed")
+#     def remove_item(self, item_name):
+#             self.items.remove(item_name)
+#             print(item_name + " removed")
 
-    def show_items(self):
-        if len(self.items) == 0:
-            print("Nothing is there in list")
+#     def show_items(self):
+#         if len(self.items) == 0:
+#             print("Nothing is there in list")
+#         else:
+#             print("Items in cart:")
+#             for item in self.items:
+#                 print(item)
+
+# cart = ShoppingCart()
+# cart.add_item("Apple")
+# cart.show_items()
+# cart.remove_item("Apple")
+# cart.show_items()
+
+
+
+# Task : Employee Bonus
+# Create class Employee:
+
+# attributes: name, salary
+# Methods:
+
+# calculate_bonus() → 10% of salary
+# display_details()
+
+
+# -------------------------------
+
+# class Employee:
+#     def __init__(self,name,salary):
+#         self.name = name 
+#         self.salary = salary 
+
+
+#     def details(self):
+#         print(self.name)
+#         print(self.salary)
+
+
+#     def calculate_bonus(self):
+#         bonus = self.salary * 0.10
+#         print("Bonus amount is" ,bonus)
+#         print(self.calculate_bonus
+#               )
+        
+  
+# e1 = Employee("harsh",66000)
+
+# e1.details()
+# e1.calculate_bonus()
+
+# ====================================================================================================================================================================       
+
+# Task : Password Checker
+# Create class PasswordChecker:
+
+# method: is_valid(password)
+# Conditions:
+
+# length >= 8
+# must contain at least 1 digit
+
+class passwordchecker:
+    def is_valid(self,password):
+        if len(password) < 1:
+            return
+
+        elif len(password)>= 8: 
+            print("INVALID make your password strong")
+
         else:
-            print("Items in cart:")
-            for item in self.items:
-                print(item)
+            print("Your password is correct")
+            print(self.is_valid)
+x = passwordchecker()
+x.is_valid("wt6")
 
-cart = ShoppingCart()
-cart.add_item("Apple")
-cart.show_items()
-cart.remove_item("Apple")
-cart.show_items()
+
+
+# Task : Movie Ticket System
+# Create class MovieTicket:
+
+# attributes: movie_name, available_seats
+# Methods:
+
+# book_ticket(count)
+# cancel_ticket(count)
+# show_available_seats()
+# Conditions:
+
+# cannot book more than available
+# cannot cancel more than booked
+
+# class Movieticket:
+#     def __init__(self,movie_name,available_seats):
+#         self.movie_name = movie_name
+#         self.available_seats = available_seats
+#     def display(self):
+#             print("MOVIE NAME :"  ,self.movie_name)
+#             print("AVAILABLE SEATS FOR THIS SHOW IS ",self.available_seats)
+
+#     def bookticket(self,count):
+#         if count > (self.available_seats):
+#             print("HOUSEFULL")
+        
+#         else:
+#             print("BOOK YOUR TICKET NOW")
+
+#     def cancle_ticket(self,count):
+#         if count > (self.available_seats):
+#             print("SORRY YOU CANNOT CANCLE MORE THAN BOOKED")
+
+#         else:
+#             print("YOUR TICKET IS CANCLED SUCESSFULLY")
+
+ 
+# P = Movieticket("ATONMENT",124)
+# P.display()
+# P.bookticket(23)
+# P.cancle_ticket(125)
+
+
+
+
